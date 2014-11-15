51860f397086647347562627807473f7afc6ca62
Yang Lei <yltt1234512@gmail.com>
2014-11-15 22:39:46 +0800

    做出了打印棋盘的函数
    rt

--- mine-sweeper/mine-sweeper/main.cpp
+++ mine-sweeper/mine-sweeper/main.cpp
@@ -2,6 +2,53 @@
 
 using namespace std;
 
+/*
+ Status
+ number 1-8 : 附近的地雷数
+ number 0   : 附近没有地雷
+ number -1  : 未翻开
+ number 9   : 插上了棋子
+ number 10  : 插上了问号
+ */
+void paintStatus(int status)
+{
+    switch (status)
+    {
+        case 0:
+        {
+            cout << " ";
+            break;
+        }
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        {
+            cout << status;
+            break;
+        }
+        case -1:
+        {
+            cout << "#";
+            break;
+        }
+        case 9:
+        {
+            cout << "X";
+            break;
+        }
+        case 10:
+        {
+            cout << "?";
+            break;
+        }
+    }
+}
+
 // the size is 50 * 50
 void paint(int (*status)[50], int col, int row)
 {
@@ -18,7 +65,7 @@ void paint(int (*status)[50], int col, int row)
         cout << "│";
         for (int j = 0; j < col; j++)
         {
-            cout << status[i][j];
+            paintStatus(status[i][j]);
             cout << "│";
         }
         cout << endl << "├";
@@ -32,7 +79,7 @@ void paint(int (*status)[50], int col, int row)
     cout << "│";
     for (int j = 0; j < col; j++)
     {
-        cout << status[row - 1][j];
+        paintStatus(status[row - 1][j]);
         cout << "│";
     }
     cout << endl << "└";
@@ -45,7 +92,11 @@ void paint(int (*status)[50], int col, int row)
 
 int main()
 {
-    int status[50][50] = {0};
-    paint(status, 10, 10);
+    int status[50][50] = {0}; //Status是经过计算的结果。也就是说，需要根据布雷计算出附近的地雷数，存在status中
+    status[0][0] = -1;
+    status[0][1] = 2;
+    status[0][3] = 10;
+    status[0][4] = 9;
+    paint(status, 15, 15);
     return 0;
 }
